f462b493910717fb8426740fb6686c4794511360
rhythm-sharma <rrhythmsharma@gmail.com>
2024-02-03 07:04:31 +0530

    updated: route file by separating authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,43 +1,7 @@
-import { PROD_URL } from "@/lib/api";
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth";
 
-export const authOptions: NextAuthOptions = {
-  pages: {
-    signIn: "/login",
-  },
-  session: {
-    strategy: "jwt",
-  },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-    }),
-  ],
-  callbacks: {
-    async jwt({ user }) {
-      await fetch(`${PROD_URL}login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: user.name,
-          email: user.email,
-          profileImage: user.image,
-        }),
-      });
-
-      return {
-        id: user?.id,
-        name: user?.name,
-        email: user?.email,
-        picture: user?.image,
-      };
-    },
-  },
-};
+import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
